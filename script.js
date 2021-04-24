75991a1ee08ae682795ad42a5272ebeb79b7a12b
Nicole <nicolewu.ncw@gmail.com>
2021-04-24 13:24:22 -0400

    Added form sent message for Contact page form

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 // Hamburger menu (show and hide)
 
-// Place hamburger menu into variable 
+// Place hamburger menu into a variable 
 const menu = document.querySelector('.hamburger');
-console.log(menu);
+//Place dropdown into a variable
 const dropdown = document.querySelector('.menu .dropdown');
-console.log(dropdown);
 
 // Add event listener to hamburger which will wait for user to click on it
 menu.addEventListener('click', function() {
     // If dropdown has a class of "showMenu," remove the class to hide the menu
     if (dropdown.classList.contains('showMenu')) {
         dropdown.classList.remove('showMenu');
-        console.log('no menu')
     // If dropdown DOES NOT have a class of "showMenu," add it to show the menu
     } else {
         dropdown.classList.add('showMenu');
-        console.log('menu')
     }
 })
 
 
 // Contact 
 
-// When the form is submitted, replace the form with a message saying "Thank you for your message! I will get back to you shortly."
+// When the form is submitted, replace the form with a message saying "Thank you for your message! We will get back to you shortly."
+
+//Place form into a variable
+const contactForm = document.querySelector('.contact form');
+console.log(contactForm);
+
+//Place submit button into a variable
+const submit = document.querySelector('.contact input[type="submit"]');
+console.log(submit);
+
+// Add event listener to submit button 
+contactForm.addEventListener('submit', function(event) {
+    // Prevent page from refreshing
+    event.preventDefault();
+    // Form should disappear and message should appear
+    contactForm.textContent = `Thank you for your message! We will get back to you shortly.`;
+    // Style the message
+    contactForm.classList.add('thankYou');
+})
